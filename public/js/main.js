8a5f17592c9bd9710c4e1d2d9c5ae697c296055e
Picuu <ltpicuuu@gmail.com>
2023-03-03 11:04:39 +0000

    feat: add content to about section

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,18 @@ function showHeader() {
 }
 
 window.addEventListener("scroll", showHeader);
+
+// SHOW SKILLS FULL BOX
+const skillsBoxs = document.querySelectorAll(".skills--grid--title");
+
+skillsBoxs.forEach((skillsBox) => {
+    skillsBox.addEventListener("click", (event) => {
+        skillsBox.classList.toggle("active");
+        const skillsContent = skillsBox.nextElementSibling;
+        if (skillsBox.classList.contains("active")) {
+            skillsContent.style.maxHeight = skillsContent.scrollHeight + "px";
+        } else {
+            skillsContent.style.maxHeight = 0;
+        }
+    });
+});
